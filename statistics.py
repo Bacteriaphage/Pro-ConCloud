b8d130906bf4bda41f3e4aa7302e1c5ac69841d2
root <root@ubuntu.ubuntu-domain>
2017-03-28 18:04:05 -0400

    add comment

--- statistics.py
+++ statistics.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import crawler
 import re
 
-class CustComm:
+class CustComm:                             #This class is a structure stores cust, stars and comment for one cust
     cust = ''# {{{
     point = 0.0
     comment = ''
@@ -14,21 +14,18 @@ class CustComm:
         pass# }}}
 crawler.start()
 dataArray = []
-fopen =  open('reviewPages/review.html')
+fopen =  open('reviewPages/review.html', 'r')
 soup = BeautifulSoup(fopen, "html.parser")
-rawData = soup.find_all(class_="a-section review")
+rawData = soup.find_all(class_="a-section review") #find all comment section in HTML
 for row in rawData:
     now = CustComm()
     now.cust = row['id']
     rawPoint = row.find('i').string
     point = re.search('(.+?) out of 5 stars', rawPoint)
     now.point = float(point.group(1))
-    rawComment = row.find('span', class_="a-size-base review-text").text
-    now.comment = rawComment.encode("UTF-8")
+    rawComment = row.find('span', class_="a-size-base review-text").text #get all comment in one block
+    now.comment = rawComment.encode("UTF-8")       #encode with UTF-8 to avoid mistake!!!
     print rawComment
-#    for e in rawComment.find_all('br'):
-#        e.decompose()
-#    now.comment = rawComment.string
     dataArray.append(now)
 for data in dataArray:
     print data.cust, data.point
