7aafcb9331bb512d10e83ab54b06b63f60ee4569
root <root@ubuntu.ubuntu-domain>
2017-03-28 20:50:26 -0400

    multithread data collection

--- crawler.py
+++ crawler.py
@@ -1,4 +1,4 @@
-import urllib2,os,sys,time
+import urllib2,os,sys,time,glob
 
 def start():
         browser = urllib2.build_opener()
@@ -6,21 +6,34 @@ def start():
         browser.addheaders=[('User-agent', 'Mozilla/5.0')]
 
         if not os.path.exists('reviewPages'):os.mkdir('reviewPages')
+        else:
+            for delfile in glob.glob('reviewPages/*.*'): #delete all prev files
+                os.remove(delfile)
+
 
         url = raw_input("input URL here: ")
         print 'start crawler...'
-        try:
-            response = browser.open(url)
-        except Exception as e:
-            error_type, error_obj, error_info = sys.exc_info()
-            print 'ERROR FOR LINK',url
-            print error_type, 'Line:', error_info.tb_lineno
+        i = 1
+        bad = False
+        for i in range(1,11):
+                try:
+                    response = browser.open(url+'&pageNumber='+str(i))
+                except Exception as e:
+                    error_type, error_obj, error_info = sys.exc_info()
+                    print 'ERROR FOR LINK',url
+                    print error_type, 'Line:', error_info.tb_lineno
+                    bad = True
+                    break
 
-        myHTML=response.read()
+                myHTML=response.read()
 
-        fwriter=open('reviewPages/'+'review.html','w')
-        fwriter.write(myHTML)
-        fwriter.close()
+                fwriter=open('reviewPages/review'+str(i)+'.html','w')
+                fwriter.write(myHTML)
+                fwriter.close()
+        if bad:
+            return i-1
+        else:
+            return i
 
 def main():
     print 'module test...'
--- statistics.py
+++ statistics.py
@@ -1,33 +1,55 @@
 from bs4 import BeautifulSoup
 import crawler
 import re
+import concurrent.futures
 
 class CustComm:                             #This class is a structure stores cust, stars and comment for one cust
-    cust = ''# {{{
-    point = 0.0
-    comment = ''
+# {{{
+    _cust = ''
+    _point = 0.0
+    _comment = ''
     def __init__(self, cust, point, comment):
-        self.cust = cust
-        self.point = point
-        self.comment = comment
+        self._cust = cust
+        self._point = point
+        self._comment = comment
     def __init__(self):
-        pass# }}}
-crawler.start()
-dataArray = []
-fopen =  open('reviewPages/review.html', 'r')
-soup = BeautifulSoup(fopen, "html.parser")
-rawData = soup.find_all(class_="a-section review") #find all comment section in HTML
-for row in rawData:
-    now = CustComm()
-    now.cust = row['id']
-    rawPoint = row.find('i').string
-    point = re.search('(.+?) out of 5 stars', rawPoint)
-    now.point = float(point.group(1))
-    rawComment = row.find('span', class_="a-size-base review-text").text #get all comment in one block
-    now.comment = rawComment.encode("UTF-8")       #encode with UTF-8 to avoid mistake!!!
-    print rawComment
-    dataArray.append(now)
-for data in dataArray:
-    print data.cust, data.point
-    print data.comment
-fopen.close()
+        pass
+# }}}
+
+def extract_info(dataArray, start, total, gap):
+# {{{
+    for i in range(start, total+1, gap):
+        fopen =  open('reviewPages/review'+str(i)+'.html', 'r')
+        soup = BeautifulSoup(fopen, "html.parser")
+        rawData = soup.find_all(class_="a-section review") #find all comment section in HTML
+        for row in rawData:
+            now = CustComm()
+            now.cust = row['id']
+            rawPoint = row.find('i').string
+            point = re.search('(.+?) out of 5 stars', rawPoint)
+            now.point = float(point.group(1))
+            rawComment = row.find('span', class_="a-size-base review-text").text #get all comment in one block
+            now.comment = rawComment.encode("UTF-8")       #encode with UTF-8 to avoid mistake!!!
+            dataArray.append(now)
+#        for data in dataArray:
+#            print data.cust, data.point
+#            print data.comment
+        fopen.close()
+    return 'finish at '+str(i)+'page'
+# }}}
+def main():
+# {{{
+    filenum = crawler.start()
+    print "crawling web page..."
+    print "generate", str(filenum), "files"
+    dataArray = []
+    print "extracting data..."
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        future_to_ext = {executor.submit(extract_info, dataArray, start, filenum, 4): start for start in range(1,5)}
+        for future in concurrent.futures.as_completed(future_to_ext):
+            print future.result()
+    print "extract",len(dataArray),"comment"
+# }}}
+if __name__ == '__main__':
+    main()
+
